9607e013df8d7fea1d96d3c5e99e850dcd58f390
Nadia Wartiningrum <65618373+nadiawaa@users.noreply.github.com>
2021-10-20 07:14:45 +0700

    Update lcd.ino

--- lcd.ino
+++ lcd.ino
@@ -1,34 +1,30 @@
 //Buat program untuk memunculkan tulisan di LCD dengan aturan :
-//➢ Ketika tombol 1 ditekan tulisan akan muncul di baris pertama LCD : tombol 1 ditekan
-//➢ Ketika tombol 2 ditekan tulisan akan muncul di baris pertama LCD : tombol 2 ditekan
+//➢ Ketika tombol 1 ditekan tulisan akan running kanan : tombol 1 ditekan
+//➢ Ketika tombol 2 ditekan tulisan akan running kiri : tombol 2 ditekan
 //➢ Fungsi lcd.clear(); untuk menghapus semua tulisan yang ada di LCD Tambahkan didalam 
-//program sehingga ketika tombol 3 ditekan tuliasan akan hilang.
-
-
 
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-int tb1=10;
-int tb2=9;
-int tb3=8;
+int tb1=10; //D10
+int tb2=9;  //D9
 
 void setup() {
  lcd.begin(16, 2);
  lcd.print("hello, world!");
  pinMode(tb1, INPUT);
  pinMode(tb2, INPUT);
- pinMode(tb3, INPUT);
+
  }
 void loop() {
  if(digitalRead(tb1)==LOW){
    for (int positionCounter = 0; positionCounter < 40; positionCounter++){
-     lcd.scrollDisplayRight();
+     lcd.scrollDisplayRight(); //running kanan
      delay(150);
    }
  }
  if(digitalRead(tb2)==LOW){
    for (int positionCounter = 0; positionCounter < 40; positionCounter++){
-     lcd.scrollDisplayLeft();
+     lcd.scrollDisplayLeft(); //running kiri
      delay(150);
    }
  }
