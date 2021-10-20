593bcd0286d7ba43fe999acbb9291eac535589de
Nadia Wartiningrum <65618373+nadiawaa@users.noreply.github.com>
2021-10-20 07:16:38 +0700

    Sensor Turbidity

--- turbidity.ino
+++ turbidity.ino
@@ -1,10 +1,9 @@
-
 void setup() {
-  Serial.begin(9600); //Baud rate: 9600
+  Serial.begin(9600);
 }
 void loop() {
   int sensorValue = analogRead(A0);// read the input on analog pin 0:
-  int turbidity = map(sensorValue, 650,940, 100, 0);
+  int turbidity = map(sensorValue, 650,940, 100, 0); //Mapping sensor, 650 nilai terkecil, 940 nilai terbesar, 100 dan 0 untuk mapping dan mengubah nilai dari 0-100
   Serial.println(turbidity); // print out the value you read:
   delay(500);
 }
