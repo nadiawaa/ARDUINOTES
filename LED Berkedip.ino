f3a36353443728b2b91302cea84e4be8404af360
Nadia Wartiningrum <65618373+nadiawaa@users.noreply.github.com>
2021-10-20 07:01:05 +0700

    Update and rename nadiakedip2.ino to LED Berkedip.ino

--- LED Berkedip.ino
+++ LED Berkedip.ino
@@ -1,6 +1,8 @@
-int ledA=12;
-int ledB=11;
-int ledC=10;
+//program untuk menyalakan LED dan mematikan LED dengan delay 2 detik
+
+int ledA=12;  //D12
+int ledB=11;  //D11
+int ledC=10;  //D10
 
 void setup(){
  pinMode(ledA, OUTPUT);
@@ -11,8 +13,9 @@ void loop(){
    digitalWrite(ledA, HIGH);
    digitalWrite(ledB, HIGH);
    digitalWrite(ledC, HIGH);
-   delay(2000);
+   delay(2000);  //delay 2 detik
    digitalWrite(ledA, LOW);
    digitalWrite(ledB, LOW);
    digitalWrite(ledC, LOW);
+   delay(2000);
 }
