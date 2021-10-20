ae691dab66c783bf8917f51c44b6277794447ff9
Nadia Wartiningrum <65618373+nadiawaa@users.noreply.github.com>
2021-10-20 07:17:19 +0700

    Sensor ultrasonic

--- ULTRASONIC.ino
+++ ULTRASONIC.ino
@@ -5,12 +5,12 @@
 NewPing sonar = NewPing(trigPin, echoPin, MAX_DISTANCE);
 
 void setup() {  
-Serial.begin(9600); 
+  Serial.begin(9600); 
 } 
 
 void loop() { 
-delay(500);  
-Serial.print("Jarak Pembacaan = "); 
-Serial.print(sonar.ping_cm()); //nilai jarak yang terbaca 
-Serial.println(" cm");  
+  delay(500);  
+  Serial.print("Jarak Pembacaan = "); 
+  Serial.print(sonar.ping_cm()); //nilai jarak yang terbaca 
+  Serial.println(" cm");  
 } 
